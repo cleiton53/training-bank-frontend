0393e264e3172c03e7df050b938a4c904432a9ca
Cleiton Alves <cleitoncaf13@gmail.com>
2020-06-01 08:51:44 -0300

    Exemplo de internet banking via angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+import { HomeLogadaComponent } from './home-logada/home-logada.component';
+import { AuthGuard } from 'auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContentComponent },
+  { path: 'cadastro-clientes', component: CadastroClientesComponent },
+  { path: 'home-logada', component: HomeLogadaComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
